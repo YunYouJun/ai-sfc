a4395ede483183f3347411d25a44488b6da30a15
YunYouJun <me@yunyoujun.cn>
2024-02-05 01:42:11 +0800

    feat: add clarity analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,6 +70,16 @@ export default defineNuxtConfig({
         { name: 'description', content: appDescription },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
       ],
+      script: [
+        {
+          type: 'text/javascript',
+          innerHTML: `    (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kwm5q1tej7");`,
+        },
+      ],
     },
   },
 
