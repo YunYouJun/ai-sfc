e1390629f110820a4368b0f1c372d20fe299c2d4
YunYouJun <me@yunyoujun.cn>
2024-02-04 18:03:28 +0800

    feat: init with vitesse-nuxt3

---
+++ README.md
@@ -0,0 +1,8 @@
+# AI Couplets - AI 对联/春联
+
+## Dev
+
+```bash
+pnpm i
+pnpm dev
+```
---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div text="xl gray4" m-5 flex="~ gap3" justify-center>
+    <NuxtLink i-carbon-campsite to="/" />
+    <a i-carbon-logo-github href="https://github.com/YunYouJun/ai-couplets" target="_blank" />
+    <DarkToggle />
+  </div>
+</template>
